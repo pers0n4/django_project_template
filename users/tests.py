876ba7025f5a188624429c2db4e1b758d3022386
Dong-Young Kim <31337.persona@gmail.com>
2021-06-24 18:45:12 +0900

    test: add user test case example

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
 
-# Create your tests here.
+from .models import User
+
+
+class UserTestCase(TestCase):
+    def setUp(self) -> None:
+        User.objects.create(username="test", password="test")
+
+    def test_create_user(self):
+        user = User.objects.get(username="test")
+        self.assertIsNotNone(user)
